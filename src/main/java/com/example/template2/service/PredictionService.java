0c6fedc9382afa965db7df0be40a29d0ae672de8
Adz1q <mikolajs2211@gmail.com>
2024-10-05 17:12:06 +0200

    Added Prediction table

---
+++ src/main/java/com/example/template2/service/PredictionService.java
@@ -0,0 +1,40 @@
+package com.example.template2.service;
+
+import com.example.template2.model.Prediction;
+import com.example.template2.model.PredictionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PredictionService {
+    private final PredictionRepository predictionRepository;
+
+    public PredictionService(PredictionRepository predictionRepository) {
+        this.predictionRepository = predictionRepository;
+    }
+
+    public List<Prediction> getPredictions() {
+        return this.predictionRepository.findAll();
+    }
+
+    public ResponseEntity<Object> getPredictionByAreaId(int areaId) {
+        Optional<Prediction> optionalPrediction = this.predictionRepository.findById(areaId);
+
+        if(!optionalPrediction.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Prediction prediction = optionalPrediction.get();
+        return ResponseEntity.ok(prediction);
+    }
+
+    //Do testowania POST requestów
+    /*public ResponseEntity<Object> createPrediction(Prediction prediction) {
+        predictionRepository.save(prediction);
+        return new ResponseEntity<>(prediction, HttpStatus.CREATED);
+    }*/
+}
